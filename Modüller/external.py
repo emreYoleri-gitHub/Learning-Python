f734f479ae06d8031da74002de2fb816617e742d
Emre Yoleri <emreyoleri01@gmail.com>
2022-06-14 14:19:06 +0300

    Working With Modules

---
+++ Modüller/external.py
@@ -0,0 +1,9 @@
+import termcolor
+
+sonuc = dir(termcolor)
+
+print(sonuc)
+""" sonuc = colored("Merhaba",color="green",on_color="on_yellow")
+sonuc = colored("Merhaba",color="green",on_color="on_yellow",attrs=["bold"])
+
+print(sonuc) """
