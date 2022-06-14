08432110dcf898fbe0ae7da575bcb9c9c3df8b78
Emre Yoleri <emreyoleri01@gmail.com>
2022-06-14 17:52:39 +0300

    Object Orienting Programming

--- Modüller/external.py
+++ Modüller/external.py
@@ -1,9 +1,8 @@
-import termcolor
+from termcolor import colored
 
-sonuc = dir(termcolor)
-
-print(sonuc)
-""" sonuc = colored("Merhaba",color="green",on_color="on_yellow")
+# sonuc = dir(termcolor)
+# sonuc = help(termcolor)
+sonuc = colored("Merhaba",color="green",on_color="on_yellow")
 sonuc = colored("Merhaba",color="green",on_color="on_yellow",attrs=["bold"])
 
-print(sonuc) """
+print(sonuc)
